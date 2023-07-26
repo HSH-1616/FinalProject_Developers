f61d7867dfc0f08a6e1c540959ffc91495df89ab
Chaneun Kim <cksdms977@naver.com>
2023-07-26 16:25:49 +0900

    초기파일세팅

---
+++ developers_finalproject/src/main/java/com/dev/DevelopersFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersFinalprojectApplication.class, args);
+	}
+
+}
