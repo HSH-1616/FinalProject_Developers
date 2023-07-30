8f3d470fecf068ffa0c81077bc3a5c6128473892
Chaneun Kim <cksdms977@naver.com>
2023-07-30 16:16:06 +0900

    api DB 등록은 할수 있는데, api 데이터가 12377개를 다 저장하려니 계속 connection time out 오류가 뜬다. 이것만 고치면 됨

---
+++ developers_finalproject/src/main/java/com/dev/touris/model/vo/Touris.java
@@ -0,0 +1,31 @@
+package com.dev.touris.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Touris {
+	private int tourisId;
+	private String tourisName;
+	private String tourisAddress;
+	private String tourisContent;
+	private String tourislatitude;
+	private String tourisLongitude;
+	private String tourisLevel;
+	private String tourismainImge;
+	private String tourispage;
+	private String tourisPhone;
+	private String tourisExperience;
+	private String tourisTel;
+	private String tourisUsemonth;
+	private String tourisUsetime;
+	private String tourisParking;
+	private String tourisStartday;
+	private String tourisDayoff;
+	private int areaId;
+}
