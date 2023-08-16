cea755643b82a8d6ebabf777128cae66a13ff942
Chaneun Kim <cksdms977@naver.com>
2023-08-16 16:41:14 +0900

    여행경로 수정하는것 수정중

--- developers_finalproject/src/main/webapp/js/mypage/mypage.js
+++ developers_finalproject/src/main/webapp/js/mypage/mypage.js
@@ -138,18 +138,59 @@ let areaName;
 		data: {loginmemberid : loginmemberid},
 		success:(data)=>{
 			console.log(data);
-			data.forEach(e =>{
+			const alldaydetailscheduleDiv = document.querySelector('.alldaydetailschedule');
+    				alldaydetailscheduleDiv.innerHTML = '';
+			data.forEach((e) =>{
 				
 				const endDate = new Date(e.tuendDate);
 				const startDate = new Date(e.tustartDate);
+				
 					// 두 날짜 사이의 일 수 차이 계산
 					const dayDifference = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
+					let daycount = (dayDifference+1);
 					
 				e.tourisArealist.forEach(e=>{
 					areaLatitude = e.areaLatitude;
 					areaLongitude = e.areaLongitude;
 					areaName = e.areaName;
 					
+					for(let i = 0; i <= dayDifference; i++) {
+						const currentDate = new Date(startDate);
+						currentDate.setDate(startDate.getDate() + i);
+
+						const dayNumber = i + 1;
+						const dayName = currentDate.toLocaleDateString('en-US', { weekday: 'short' });
+						const month = currentDate.toLocaleDateString('kr-KR', { month: 'short' });
+						const day = currentDate.getDate();
+						
+						
+					const newElement = document.createElement('div');
+					newElement.classList.add('detailroutetour-' + dayNumber); // 
+					newElement.id = 'detailroutetareaid';
+
+					newElement.innerHTML = '<div class="alldayinputfield">' +
+						'  <div id="dayselectbox" class="dayselecttag">' +
+						 	 + dayNumber + '일차 ' + month + ' ' + day + '일 ' + dayName +
+						'  </div>' +
+						'</div>' +
+						'<div class="detailroutetext"><hs>일차를 누르면 일정 전체 변경이 가능합니다.</hs></div>' +
+						'<div class="dailyroutedetaillist">' +
+						'  <div class="dailyroutecountarea">' +
+						'    <div class="dailyroutetext">' +
+						'      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">' +
+						'        <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />' +
+						'      </svg>' +
+						'      <div class="dailytext1"></div>' +
+						'      <div class="dailytext2">장소</div>' +
+						'    </div>' +
+						'  </div>' +
+						'  <hr class="hr2"></hr>' +
+						'  <div class="routedatailslists" id="routedatails">' +
+						'    </div>' +
+						'  </div>' +
+						'</div>';
+
+					alldaydetailscheduleDiv.appendChild(newElement);
 					
 					
 					
@@ -157,6 +198,8 @@ let areaName;
 					
 					
 					
+					
+				}
 					
 					
 					
